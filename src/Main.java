6f676d1449d5625f584d309e30883c4dc42c9b06
OrionMaster <masterrf@gmail.com>
2022-06-29 19:12:16 +0300

    next

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,34 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Integer> arr2 = new ArrayList<>();
 
+        for (int i = 0; i < 100; i++) {
+            arr2.add( (int) ( 33 + Math.random() * (77  + 1 - 33 ))  );
+        }
+
+        for (int a = 0; a < arr2.size() ; a++) {
+            for (int i = 0; i < arr2.size() -1; i++) {
+                if (dalybaBeLiekanos(arr2.get(i)) > dalybaBeLiekanos(arr2.get(i + 1))) {
+                    int t1 = arr2.get(i);
+                    arr2.set(i, arr2.get(i + 1));
+                    arr2.set(i + 1, t1);
+                }
+            }
+        }
+
+
+        public static int dalybaBeLiekanos(int duotasSkaicius) {
+            int sveikasSkaicius = 0;
+
+            for (int i = 2; i < duotasSkaicius; i++) {
+                if ((duotasSkaicius % i) == 0) {
+                    sveikasSkaicius++;
+                } else sveikasSkaicius = sveikasSkaicius;
+            }
+
+            return sveikasSkaicius;
+        }
 
 
 
