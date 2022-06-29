3aa843a06beb8575ecb0eb7919eda06b3d61c1a3
OrionMaster <masterrf@gmail.com>
2022-06-29 20:02:06 +0300

    3 students

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,41 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        Student studentas = new Student();
+        studentas.age = 17;
+        studentas.name = "Juozapas";
+        studentas.surname = "Pavardenis";
+        studentas.grades.add(9);
+        studentas.grades.add(10);
 
+        System.out.println(studentas.name + " " + studentas.surname);
+
+
+        Student studentas2 = new Student();
+        studentas2.name = "Jonas";
+        studentas2.surname = "Jonaitis";
+        studentas2.age = 18;
+        System.out.println(studentas2.name + " " + studentas2.surname);
+
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(studentas);
+        students.add(studentas2);
+
+        Student studentas3 = new Student( "Algis", "Raman", 54 );
+        students.add(studentas3);
+
+        for (int i = 0; i < students.size(); i++) {
+            Student student = students.get(i);
+            System.out.println(student.name + " " + student.surname + " ");
+            for (int a = 0; a < students.get(i).grades.size(); a++) {
+                int grade = student.grades.get(a);
+                System.out.println(students.get(i).grades.get(a) + ", ");
+                System.out.print(grade + ", ");
+
+            }
+            System.out.println();
+    }
 
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    String name;
+    String surname;
+    int age;
+    ArrayList<Integer> grades;
+    public Student() {
+        this.grades = new ArrayList<>();
+    }
+        public Student(String name, String surname, int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.grades = new ArrayList<>();
+
+
+    }
+
+
+
+    }
+
