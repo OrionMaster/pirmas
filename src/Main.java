ee44f3687ed91fcdc08dfb38b82c70d8027aaca2
OrionMaster <masterrf@gmail.com>
2022-06-30 20:54:08 +0300

    inputas

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,42 @@ public class Main {
             try {
                 inputInt = sc.nextInt();
             }catch (Exception e){
-                System.out.println("wrong input format");
+                System.out.println(e);
             }
 
             System.out.println("Labai gerai, tu pasakei " + inputInt);
 
+/*      Vilniaus studentu registracijos systema
+        Tam reikes:
+        Objectines klases "Student"
+        Jos privat laukai: name, surname, age, phone, email
+        konstruktorius - tuscias
+        geters (public)
+        main klase pasinaudoje scanner surinkite studentu duomenis po viena:
+        vardas ? <- varda
+        pavarde ? <- pavarde
+        Studentus saugome
 
+        lv1
+        suvedus pilnai studenta - ji isspauzdinti su toString override metod
+
+        lv2
+        sudeti visas apsaugas. Jai ivedei bloga amziu, praso dar karta, kol neives teisingai()while ciclas)
+        keleta studentu. Massivas(ArrayList)  (didelis while)
+
+        lv3
+        reikes pagalbiniu klase su statiniais metodais pavadinimu  (Validation)
+        metodai validuoti: laukiu ivedima
+
+        Validation.name(sc.input)
+
+        Lv4
+        Validacija lauku: phone ( +(370), 86, 6 ) ir mail ("@", "@ + ." , ". + dsgs")
+
+        lv5
+
+
+ */
 
         }
 
